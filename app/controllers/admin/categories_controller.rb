477cb1c5f7095aaef7b2ac23cae6cb98805308b4
Junsung Park <zeipar@gmail.com>
2020-05-06 08:13:33 +0000

    Fixed private method using param :product -> category.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -23,7 +23,7 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:product).permit(
+    params.require(:category).permit(
       :name
     )
   end
