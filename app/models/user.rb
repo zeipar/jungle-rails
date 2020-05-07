1aeb5e74ef33d5efadd5d2060e94f581a37b9e40
Junsung Park <zeipar@gmail.com>
2020-05-07 05:10:06 +0000

    Added model rspec testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.email.downcase
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    # whitespace padding removed and lowercased
+    base_email = email.strip.downcase
+    user = User.find_by_email(base_email)
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+
 end
