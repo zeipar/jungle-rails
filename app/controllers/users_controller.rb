6af7a27a00da7fc2faf6eebfbb4b6e37a51adafc
Junsung Park <zeipar@gmail.com>
2020-05-07 01:31:32 +0000

    Added to show login status to top nav. Fixed user controller with wrong number of <end>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,11 @@ class UsersController < ApplicationController
     else
       redirect_to '/signup'
     end
-
-    private
-      def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
